wblixin6017/google-fuchsia-magenta

start on virtual USB
Change-Id: Id8bd23580a56ac0b54f168370e17a96bf7c63aae

system/public/magenta/device/ioctl.h
@@ -54,6 +54,7 @@
 #define IOCTL_FAMILY_RAMDISK        0x22
 #define IOCTL_FAMILY_SDMMC          0x23
 #define IOCTL_FAMILY_WLAN           0x24
+#define IOCTL_FAMILY_USB_CLIENT     0x25
 
 // IOCTL constructor
 // --K-FFNN

zhuzhengchao/magenta

system/public/magenta/device/ioctl.h
@@ -57,6 +57,7 @@
 #define IOCTL_FAMILY_WLAN           0x24
 #define IOCTL_FAMILY_PTY            0x25
 #define IOCTL_FAMILY_NETCONFIG      0x26
+#define IOCTL_FAMILY_USB_CLIENT     0x27
 
 // IOCTL constructor
 // --K-FFNN